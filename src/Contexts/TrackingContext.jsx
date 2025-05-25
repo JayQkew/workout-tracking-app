d42ff5aa78531a1f0633d4b5727da7a8cb8b8bbc
JayQkew <rayjebb@gmail.com>
2025-05-25 17:48:44 +0200

    setting plan for tracking

--- src/Contexts/TrackingContext.jsx
+++ src/Contexts/TrackingContext.jsx
@@ -1,6 +1,6 @@
 import { createContext, useContext, useState } from "react";
 import { useWorkout } from "./WorkoutContext";
-import tracker from '../workout-tracking.json';
+import _tracker from '../workout-tracking.json';
 
 const TrackingContext = createContext();
 
@@ -8,12 +8,33 @@ const TrackingContext = createContext();
 export function TrackingProvider({ children }) {
     const { plan, session } = useWorkout();
 
+    const [tracker, setTracker] = useState(_tracker);
+
+    const [trackPlan, setTrackPlanState] = useState(plan);
+    const [trackSession, setTrackSession] = useState(plan.sessions[0]);
+
+    // Custom setter
+    function setTrackPlan(newPlan) {
+        const p = {
+            id: newPlan.id,
+            name: newPlan.name,
+            startDate: '',
+            sessions: []
+        }
+
+        const exists = tracker.some(p => p.id === newPlan.id);
+        if (!exists) {
+            setTracker([...tracker, p]);
+        }
+        setTrackPlanState(p);
+    }
+
     function addWeightReps(exercise, weightReps){
 
     }
 
     return (
-        <TrackingContext.Provider value={{ addWeightReps }}>
+        <TrackingContext.Provider value={{ tracker, addWeightReps, setTrackPlan }}>
             {children}
         </TrackingContext.Provider>
     );
