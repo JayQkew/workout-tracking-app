91d3f326d5b9cfe309e9701bcab22d4fdd4d2149
JayQkew <rayjebb@gmail.com>
2025-06-01 20:13:27 +0200

    finished home page

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -7,21 +7,19 @@ function Home(){
             <header className="page-header landing-section">
                 <h1>Lets Get To <br/> <GlitchWord>Work</GlitchWord></h1>
                 <p className='subtitle'>A light weight solution for workout tracking and planning</p>
-                <button>Get Started</button>
+                <button className='action-btn'>Get Started</button>
             </header>
-            <section>
-                <h2>Features</h2>
+            <section className='home-section'>
                 <ul>
                     <li>Track your workouts</li>
                     <li>Plan your training sessions</li>
                     <li>View your progress</li>
-                    <li>Customizable settings</li>
                 </ul>
             </section>
-            <section>
-                <h2>Lets Get To Work!</h2>
-                <p>Jump right in! the weights wait for no-one</p>
-                <button>Yessir!</button>
+            <section className='home-section'>
+                <h2>Organised and Minimalistic</h2>
+                <p>Jump right in! The weights wait for no-one</p>
+                <button className='action-btn'>Yessir!</button>
             </section>
         </>
     )
