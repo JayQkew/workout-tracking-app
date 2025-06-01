33ab2f99fca21885d516ec4e70df27d15c137697
JayQkew <rayjebb@gmail.com>
2025-06-01 22:03:12 +0200

    finish adding stickers

--- src/Components/PlanSticker/PlanSticker.jsx
+++ src/Components/PlanSticker/PlanSticker.jsx
@@ -1,7 +1,13 @@
+import './PlanSticker.css';
+
 function PlanSticker(props){
     const { session } = props;
 
-    return session.type === 'gym' ? <div>o</div> : <div>x</div>;
+    return (
+        <div className={session.type === 'gym' ? 'sticker gym-session' : 'sticker rest-session'}>
+            {session.type === 'gym' ? 'gym' : 'rest'}
+        </div>
+    );
 }
 
 export default PlanSticker
