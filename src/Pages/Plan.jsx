ed9ccf20cbc7a38b736f12111a4890550b1dede8
JayQkew <rayjebb@gmail.com>
2025-06-03 11:37:49 +0200

    adding styling to the all session header

--- src/Pages/Plan.jsx
+++ src/Pages/Plan.jsx
@@ -3,6 +3,7 @@ import '../Styles/PageLayout.css';
 import PageHeader from "../Components/PageHeader/PageHeader";
 import AllPlans from "../Components/AllPlans/AllPlans";
 import PlannedSession from '../Components/PlannedSession/PlannedSession';
+import GlitchWord from '../Components/GlitchWord/GlitchWord';
 
 import { useWorkout } from '../Contexts/WorkoutContext';
 
@@ -28,6 +29,7 @@ function Plan(){
                     <AllPlans/>
                 </aside>
                 <section className="main-cards">
+                    <h2>{plan.name}</h2>
                     {plan.sessions.map((s, i) => (
                         <PlannedSession 
                             session={s} 
